df7fceee0b764e5fdaf7e71efa5dd44f367082fd
Braachio <166890773+Braachio@users.noreply.github.com>
2025-06-04 11:49:20 +0900

    Add root layout for Next.js app directory

--- components/MultiListPage.tsx
+++ components/MultiListPage.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect, useState } from 'react'
-import Link from 'next/link'
 import MultiCard from './MultiCard'
 import type { Database } from '@/lib/database.types'
 
