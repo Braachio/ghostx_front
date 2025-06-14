f641b5ddc4c692e81a80b89297846b1562e277d8
Braachio <166890773+Braachio@users.noreply.github.com>
2025-06-14 14:30:27 +0900

    🎨 스타일 수정 및 멀티 등록 폼 개선

--- app/multis/edit/[id]/page.tsx
+++ app/multis/edit/[id]/page.tsx
@@ -1,9 +1,6 @@
 import EditMultiForm from '@/components/EditMultiForm'
 
-interface Props {
-  params: { id: string }
-}
-
-export default function Page({ params }: Props) {
+// Next.js는 기본적으로 아래와 같은 형태로 expects 합니다.
+export default function Page({ params }: { params: { id: string } }) {
   return <EditMultiForm id={params.id} />
 }
